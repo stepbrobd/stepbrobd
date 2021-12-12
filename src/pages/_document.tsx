6f4b67a1c9579c18ce9dba315b6f456a8a973e9b
Yifei Sun <ysun@hey.com>
2021-12-12 14:34:14 -0700

    init

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import {Head, Html, Main, NextScript} from 'next/document'
+
+const Document = () => {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta charSet="UTF-8"/>
+                <meta name="theme-color" content="#000000"/>
+                <meta name="description" content="StepBroBD"/>
+                <link rel="icon" type="image/png" href="/favicon.ico"/>
+                <meta property="og:image" content="/og.png"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
+}
+
+export default Document
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['-apple-system', '"Roboto"', 'sans-serif']
+        },
+        extend: {},
+    },
+}
