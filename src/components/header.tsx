b3b3b9132b8b94114a842f8db53dcd9500acc985
Yifei Sun <ysun@hey.com>
2021-12-20 22:32:51 -0700

    added header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import {FunctionComponent} from "react"
 const Header: FunctionComponent = () => {
     return (
         <header className="border-b-2 border-neutral-700">
-            <div className="container flex flex-row justify-center items-center px-3 py-3">
+            <div className="container flex flex-row justify-center items-center mx-auto px-3 py-3">
                 <Link href="/">
                     <a className="text-3xl">
                         <span>StepBroBD</span>
