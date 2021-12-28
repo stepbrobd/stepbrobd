c00e27a7d694eb089ec61a3b4d5964d87e1107db
Yifei Sun <ysun@hey.com>
2021-12-27 21:59:07 -0700

    changed layout usage

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,30 +1,21 @@
 import type {ReactNode} from "react"
 
-import Head from "next/head"
-
-import {Footer, Header, Meta} from "./components"
+import {Footer, Header} from "./components"
 
 type Props = {
-    title: string
-    description: string
     children?: ReactNode
 }
 
-const Layout = ({title, description, children}: Props) => {
+const Layout = ({children}: Props) => {
     return (
         <>
-            <Meta/>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={description}/>
-            </Head>
+            <Header/>
             <div className="selection:bg-yellow-400 selection:text-black">
-                <Header/>
                 <main className="px-6 py-6">
                     {children}
                 </main>
-                <Footer/>
             </div>
+            <Footer/>
         </>
     )
 }
