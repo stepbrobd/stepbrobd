1e1e3b666a5f9d1c025c833c78c0d87cccd0cee5
Yifei Sun <ysun@hey.com>
2021-12-27 22:02:01 -0700

    blog index page init

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,14 @@
+import {Layout, Meta} from "../../components/components"
+
+const BlogIndex = () => {
+    return (
+        <>
+            <Meta title="StepBroBD" description="StepBroBD homepage."/>
+            <Layout>
+
+            </Layout>
+        </>
+    )
+}
+
+export default BlogIndex
