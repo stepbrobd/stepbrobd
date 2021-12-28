5ea05c46dff5ad2a2b0ff84816328b799cee6ab4
Yifei Sun <ysun@hey.com>
2021-12-27 20:55:40 -0700

    renamed

---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+import {Layout} from "../components/components"
+
+const About = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>GPG | StepBroBD</title>
+                <meta name="description" content="StepBroBD"/>
+            </Head>
+        </Layout>
+    )
+}
+
+export default About
