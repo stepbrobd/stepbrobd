de68b7ec476a4cc8f647692db94729b7573b736f
Yifei Sun <ysun@hey.com>
2021-12-28 21:47:13 -0700

    added custom error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,33 @@
+import {NextPageContext} from "next"
+import Image from "next/image"
+
+import {Layout, Meta} from "../components/components"
+
+type Props = {
+    statusCode?: number
+}
+
+const Error = ({statusCode}: Props) => {
+    return (
+        <>
+            <Meta title={statusCode + " | StepBroBD"} description={"An error with status " + statusCode + " occurred."}/>
+            <Layout>
+                <div className="not-prose flex flex-col justify-center items-center">
+                    <h1 className="font-black text-3xl pt-8 pb-4">Oops...</h1>
+                    <Image src={"https://static.stepbrobd.com/status/" + statusCode + ".jpg"}
+                           width={750}
+                           height={600}
+                           alt={"Error" + statusCode}
+                    />
+                </div>
+            </Layout>
+        </>
+    );
+}
+
+Error.getInitialProps = ({res, err}: NextPageContext) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return {statusCode}
+};
+
+export default Error
