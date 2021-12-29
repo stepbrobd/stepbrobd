23a141fa6694bc5e2077da50331732ed79a4c4bd
Yifei Sun <ysun@hey.com>
2021-12-28 22:30:01 -0700

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+
+import {Layout, Meta} from "../components/components"
+
+const Error = () => {
+    return (
+        <>
+            <Meta title="404 | StepBroBD" description="An error with status 404 occurred."/>
+            <Layout>
+                <div className="not-prose flex flex-col justify-center items-center">
+                    <h1 className="font-black text-3xl pt-8 pb-4">It&#39;s your fault.</h1>
+                    <Image
+                        src={"https://static.stepbrobd.com/status/404.jpg"}
+                        width={750}
+                        height={600}
+                        alt="Error 404"
+                    />
+                </div>
+            </Layout>
+        </>
+    );
+}
+
+export default Error
