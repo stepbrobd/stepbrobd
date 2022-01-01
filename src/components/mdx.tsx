87e2f6fded18612e19993a47a226ffbedd4cc4ab
Yifei Sun <ysun@hey.com>
2022-01-01 00:42:16 -0700

    mdx styling init

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,7 +1,18 @@
-const MDX = () => {
-    return (
-        <article className="prose prose-invert prose-h1:font-black prose-pre:bg-neutral-900">
+import type {ReactNode} from "react"
 
+type Props = {
+    children?: ReactNode
+}
+
+const MDX = ({children}: Props) => {
+    return (
+        <article className="
+        prose prose-invert
+        prose-h1:font-black
+        prose-pre:bg-neutral-900
+        "
+        >
+            {children}
         </article>
     )
 }
