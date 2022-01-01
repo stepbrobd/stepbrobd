623412c5448c9be4cfb7f0ef5c5332b2723f4a8d
Yifei Sun <ysun@hey.com>
2022-01-01 00:41:57 -0700

    added blog slug support

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,32 +1,82 @@
 import fs from "fs"
 import path from "path"
 
-import {Layout, Meta} from "../../components"
+import {useRouter} from "next/router"
 
-import MDXComponent from "../../mdx/blog/index.mdx"
+import {remark} from "remark"
+import html from "remark-html"
+
+import {Layout, MDX, Meta} from "components"
+
+type Props = {
+    content: string
+}
+
+type Params = {
+    params: {
+        slug: string
+    }
+}
+
+export const markdownToHtml = async (markdown: string) => {
+    const result = await remark().use(html).process(markdown)
+    return result.toString()
+}
 
 export const getStaticPaths = async () => {
-    const postFileNames = fs.readdirSync(path.join(process.cwd(), "src/mdx/blog"))
+    const items = fs.readdirSync(path.join(process.cwd(), "src/mdx", "/blog")).map(item => {
+        if (item.includes(".mdx")) {
+            if (item === "index.mdx")
+                return "/"
+            return item.replace(".mdx", "")
+        }
+    }).filter(item => !!item)
     return {
-        paths: postFileNames.map((postFileName) => ({
-            slug: postFileName.replace(/\.mdx/, '')
-        })),
+        paths: items.map(item => {
+            return {
+                params: {
+                    slug: item
+                }
+            }
+        }),
         fallback: false
     }
 }
 
-export const getStaticProps = async () => {
-
+export const getStaticProps = async ({params}: Params) => {
+    const mdxContent = fs.readFileSync(path.join(process.cwd(), "src/mdx", "/blog", params.slug + ".mdx"), "utf-8")
+    const htmlContent = await markdownToHtml(mdxContent || '')
+    return {
+        props: {
+            content: htmlContent,
+        },
+    }
 }
 
-const Blog = () => {
-    console.log(fs.readdirSync(path.join(process.cwd(), "src/mdx/blog", "")))
+const Blog = ({content}: Props) => {
+    const router = useRouter()
     return (
         <>
-            <Meta title="Blog | StepBroBD" description="StepBroBD blog."/>
-            <Layout>
-                <MDXComponent/>
-            </Layout>
+            {router.isFallback
+                ? (
+                    <>
+                        <Meta title="Loading..." description="Loading..."/>
+                        <Layout>
+                            Loading...
+                        </Layout>
+                    </>
+                )
+                : (
+                    <>
+                        <Meta title="Blog - StepBroBD" description="StepBroBD blog."/>
+                        <Layout>
+                            <MDX>
+                                <div className="container" dangerouslySetInnerHTML={{__html: (content)}}/>
+                            </MDX>
+                        </Layout>
+                    </>
+                )
+            }
         </>
     )
 }
