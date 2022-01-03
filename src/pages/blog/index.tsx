52dbff48e1dee3152e98a9fea75758e2efedd3c3
Yifei Sun <ysun@hey.com>
2022-01-03 11:19:39 -0700

    added blog post support

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -38,17 +38,27 @@ const Blog = ({slugs}: Props) => {
                     <>
                         <Meta title="Blog - StepBroBD" description="StepBroBD blog posts."/>
                         <Layout>
-                            <ul>
-                                {slugs.map((slug) => (
-                                    <li key={slug}>
-                                        <Link
-                                            href={"/blog/" + slug}
-                                        >
-                                            <a>{generateTitle(slug)}</a>
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
+                            <div className="container prose prose-invert">
+                                <div className="flex justify-start items-center">
+                                    <div className="whitespace-nowrap inline text-3xl font-black">
+                                        <h3 className="whitespace-nowrap inline font-black text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-blue-500">PO</h3>
+                                        <h3 className="whitespace-nowrap inline font-black text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500">S</h3>
+                                        <h3 className="whitespace-nowrap inline font-black text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-500">T</h3>
+                                        <h3 className="whitespace-nowrap inline font-black text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-red-500">S</h3>
+                                    </div>
+                                </div>
+                                <ul className="not-prose list-disc">
+                                    {slugs.map((slug) => (
+                                        <li key={slug} className="text-xl pt-5">
+                                            <Link
+                                                href={"/blog/" + slug}
+                                            >
+                                                <a>{generateTitle(slug)}</a>
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
                         </Layout>
                     </>
                 )
