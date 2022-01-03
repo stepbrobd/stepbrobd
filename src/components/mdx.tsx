185e29606b1fa1a9f2c8c9d5fbf8f4dd7f751829
Yifei Sun <ysun@hey.com>
2022-01-03 10:26:06 -0700

    added blog post support

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -7,6 +7,7 @@ type Props = {
 const MDX = ({children}: Props) => {
     return (
         <article className="
+        container
         prose prose-invert
         prose-h1:font-black
         prose-pre:bg-neutral-900
