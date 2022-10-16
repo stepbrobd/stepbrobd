1afc6da8fdf2d2a0455290f615536fb2be74038a
Yifei Sun <ysun@hey.com>
2022-10-16 11:49:46 -0600

    config update

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,5 @@
 /** @type {import("next-sitemap").IConfig} */
-
 module.exports = {
-    siteUrl: "https://stepbrobd.com",
-    generateRobotsTxt: true,
+  siteUrl: "https://stepbrobd.com",
+  generateRobotsTxt: true,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,32 @@
 module.exports = {
-    content: ["./src/**/*.{ts,tsx,mdx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Roboto", "sans-serif"],
-                sansSerif: ["Roboto", "sans-serif"],
-            },
-            animation: {
-                blob: "blob 10s infinite",
-            },
-            keyframes: {
-                blob: {
-                    "0%": {
-                        transform: "translate(0px, 0px) scale(1)",
-                    },
-                    "33%": {
-                        transform: "translate(30px, -50px) scale(1.1)",
-                    },
-                    "66%": {
-                        transform: "translate(-20px, 20px) scale(0.9)",
-                    },
-                    "100%": {
-                        transform: "tranlate(0px, 0px) scale(1)",
-                    },
-                },
-            },
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        sansSerif: ["Inter", "sans-serif"],
+      },
+      animation: {
+        blob: "blob 10s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
         },
+      },
     },
-    plugins: [
-        require("@tailwindcss/typography"),
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/line-clamp"),
-        require("@tailwindcss/aspect-ratio"),
-    ],
+  },
+  plugins: [],
 };
