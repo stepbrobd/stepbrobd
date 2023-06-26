a59562f32348ed2d4d8c1811b4e3d81fc96c8242
Yifei Sun <ysun@hey.com>
2023-06-26 17:54:26 -0600

    sticky header

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,17 @@
 import Logo from "$components/logo.tsx";
+import IconMenu from "icons/tsx/menu.tsx";
+import IconChevronDown from "icons/tsx/chevron-down.tsx";
 
 const Header = () => {
   return (
-    <header class="w-full py-4 px-8 flex flex-row items-center justify-between backdrop-filter backdrop-blur backdrop-brightness-75">
+    <header class="sticky top-0 w-full py-2 px-8 flex flex-row items-center justify-between backdrop-filter backdrop-blur backdrop-brightness-75">
       <div class="flex items-center justify-start h-8">
         <Logo size="small" />
       </div>
       <div class="flex items-center justify-end h-8">
-        <a href="/" class="font-bold">
-          Home
-        </a>
+        <div class="h-8 w-8 flex items-center justify-center">
+          <IconMenu />
+        </div>
       </div>
     </header>
   );
